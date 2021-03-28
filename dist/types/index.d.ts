6035da6e8dcc3a4385e819ef5dc1c97a3cd6e6d1
oz- <benoit.lahoz@carasuelo.org>
2021-03-28 19:02:57 +0200

    Fisrt commit.

---
+++ dist/types/index.d.ts
@@ -0,0 +1,12 @@
+/**
+ * The default theme.
+ * @module DefaultTheme
+ *
+ * @see https://shouts.dev/create-multiple-themes-in-vuejs
+ */
+export declare var Theme: {
+    id: string;
+    name: string;
+    sass: string;
+    css: string;
+};
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+/**
+ * The default theme.
+ * @module DefaultTheme
+ * 
+ * @see https://shouts.dev/create-multiple-themes-in-vuejs
+ */
+
+/**
+ * An unique identifier for this theme, to be set by each theme module.
+ * @constant
+ * @memberof module:DefaultTheme
+ */
+const THEME_ID = 'core.default'
+/**
+ * The unique displayable name of the theme.
+ * @constant
+ * @memberof module:DefaultTheme
+ */
+const THEME_NAME = 'Default (dark)'
+
+export const Theme = {
+  id: THEME_ID,
+  name: THEME_NAME,
+  // Paths relative to dist folder
+  paths: {
+    sass: 'sass/theme.sass',
+    css: 'css/theme.css',
+    install: `archive/theme.${THEME_NAME}.zip`
+  }
+}
